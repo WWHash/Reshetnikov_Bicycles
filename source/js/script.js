b403d0af1ffcc277903f3708f9312ff568b3bcc6
WWHash <Reshetnikov.Hash@yandex.ru>
2021-03-24 16:49:34 +0300

    Готово к проверке, исправил замечавния наставника

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@
 
 var navBtn = document.querySelector('.nav__btn');
 var nav = document.querySelector('.nav');
+var body = document.querySelector('.page-body');
 
 nav.classList.remove('nav--nojs');
 
@@ -9,8 +10,10 @@ navBtn.addEventListener('click', function () {
   if (nav.classList.contains('nav--closed')) {
     nav.classList.remove('nav--closed');
     nav.classList.add('nav--opened');
+    body.classList.add('page-body--overflow-y');
   } else {
     nav.classList.add('nav--closed');
     nav.classList.remove('nav--opened');
+    body.classList.remove('page-body--overflow-y');
   }
 });
